c2dec880dc5fdde8e68dd93cac32810e0bdc6d3d
Jhojan_Ropero <jhojanropero0@gmail.com>
2023-01-31 14:14:40 -0500

    Alien add

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 #? Inicializar
 pygame.init()
@@ -19,7 +20,16 @@ class Personaje():
   def show(self):
     pantalla.blit(self.set_image(), (self.x, self.y))
   
-  
+class Enemigo(Personaje):
+  movimiento = (2,.1) if random.randint(1,2) == 1 else (-2,.1)
+
+  def __init__(self, image, x, y):
+    self.image = f'assets/{image}'
+    self.x = random.randint(0,736)
+    self.y = random.randint(0, 300)
+    # self.x = x
+    # self.y = y
+
 
 #? Configuraciones generales
 icono = pygame.image.load('assets/favicon.svg')
@@ -32,6 +42,9 @@ pygame.display.set_icon(icono)
 #? Personaje
 nave = Personaje('nave.png', 368, 500)
 
+#? Enemigo
+alien = Enemigo('ovni.png', 0, 0)
+
 
 #? Inicializacion juego
 run = True
@@ -42,9 +55,9 @@ while run:
   for event in pygame.event.get():
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_LEFT:
-        nave.movimiento = -1
+        nave.movimiento = -2.3
       elif event.key == pygame.K_RIGHT:
-        nave.movimiento = 1
+        nave.movimiento = 2.3
 
     elif event.type == pygame.KEYUP:
       if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -54,12 +67,21 @@ while run:
     if event.type == pygame.QUIT:
       run = False
 
-  #? Movimiento
+  #? Movimiento Jugador
   nave.x += nave.movimiento
   if nave.x <= 0:
     nave.x = 0
   elif nave.x >= 736:
     nave.x = 736
+
+  #? Movimiento Alien
+  alien.show()
+  alien.x += Enemigo.movimiento[0]
+  alien.y += Enemigo.movimiento[1]
+  if alien.x >= 736:
+    Enemigo.movimiento = (-2, .1)
+  if alien.x <= 0:
+    Enemigo.movimiento = (2, .1)
 
 
   nave.show()
